4d428022cb1ce5fd64a75472ef609203260d9aaa
Luana Maistro <luanamaistro@gmail.com>
2026-02-12 00:42:43 -0300

    feat: adicionando horario da postagem também

--- frontend/blog-frontend/src/app/posts/[id]/page.tsx
+++ frontend/blog-frontend/src/app/posts/[id]/page.tsx
@@ -33,6 +33,8 @@ export default function PostDetailPage() {
     day: '2-digit',
     month: 'long',
     year: 'numeric',
+    hour: '2-digit',
+    minute: '2-digit',
   });
 
   return (
--- frontend/blog-frontend/src/presentation/components/ui/PostCard.tsx
+++ frontend/blog-frontend/src/presentation/components/ui/PostCard.tsx
@@ -20,6 +20,8 @@ export function PostCard({ post, showActions, onDelete }: PostCardProps) {
     day: '2-digit',
     month: 'long',
     year: 'numeric',
+    hour: '2-digit',
+    minute: '2-digit',
   });
 
   return (
